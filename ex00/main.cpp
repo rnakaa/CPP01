6d985f07c73600a3bb703c6e16ba7022af05bda0
rnakaa <rnaka@c1r27s10.42tokyo.jp>
2024-01-04 15:00:32 +0900

    transfer

---
+++ ex00/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+class Zombie{
+	privae:
+		std::string _name;
+	public:
+		Zombie(std::strig name);
+		~Zombie(void);
+		void setname(std::string name);
+		void announce(void);
+};
+
+
+int main(){
+	
+}
