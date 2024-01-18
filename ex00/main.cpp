6bdc7e46cadbe19a60e3a79b9e1fc9c035a1c454
rnakaa <ren0508lotus@gmail.com>
2024-01-18 15:28:43 +0900

    main file done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
 #include <string>
 
-
 class Zombie{
-	privae:
+	private:
 		std::string _name;
 	public:
-		Zombie(std::strig name);
-		~Zombie(void);
+		Zombie(std::string name);
+		Zombie();
+		~Zombie();
 		void setname(std::string name);
 		void announce(void);
 };
 
+Zombie::Zombie(std::string name){
+	std::cout << "constractor" << std::endl;
+	_name = name;
+}
+
+Zombie::Zombie(){
+	std::cout << "constractor" << std::endl;
+}
+
+Zombie::~Zombie(){
+	std::cout << "destractor" << std::endl;
+	std::cout << _name << ": BraiiiiiinnnzzZ..." << std::endl;
+}
+
+void Zombie::setname(std::string name){
+	_name = name;
+}
+
+void Zombie::announce(void){
+	std::cout << _name << ": BraiiiiiinnnzzZ..." << std::endl;
+}
 
 int main(){
-	
+	Zombie *zombie;
+
+	zombie = new Zombie();
+	zombie->setname("pip");
+	zombie->announce();
+	delete zombie;
 }
+
