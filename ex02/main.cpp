201c3bd93624b226b134149dd2da504b25535c77
rnakaa <ren0508lotus@gmail.com>
2024-01-22 18:31:03 +0900

    make ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+	std::string str = "HI THIS IS BRAIN";
+	std::string *strinPTR = &str;
+	std::string &strinREF = str;
+
+	std::cout << "address" << std::endl;
+	std::cout << "str      = " << &str << std::endl;
+	std::cout << "strinPTR = " << &*strinPTR << std::endl;
+	std::cout << "strinREF = " << &strinREF << std::endl << std::endl;
+
+	std::cout << "value" << std::endl;
+	std::cout << "str      = " << str << std::endl;
+	std::cout << "strinPTR = " << *strinPTR << std::endl;
+	std::cout << "strinREF = " << strinREF << std::endl;
+
+	return (0);
+}
+
