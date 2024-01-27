8f02aaf3602e32b583e98622b29f8f40acc86142
rnakaa <ren0508lotus@gmail.com>
2024-01-27 10:48:30 +0900

    transfer

---
+++ ex05/Harl.hpp
@@ -0,0 +1,27 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+
+class Harl{
+	private:
+		void debug(void);
+		void info(void);
+		void warming(void);
+		void error(void);
+	public:
+		Haral();
+		~Haral();
+		void complain(std::string level);
+}
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <string>
+#endif
---
+++ ex05/main.cpp
@@ -0,0 +1,8 @@
+#include "Harl.hpp"
+
+typedef int(Haral::*MEMBER_FUNC)(void)
+
+int main(){
+//	Haral *pA = new Haral();
+	(&Haral::debug)();
+}
