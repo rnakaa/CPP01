e6242bd023ea22155ad9c155d1752e33e996d339
rnakaa <ren0508lotus@gmail.com>
2024-02-03 11:27:09 +0900

    fixe ex01

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -10,7 +10,12 @@ HumanB::~HumanB(){
 }
 
 void HumanB::attack(){
-	std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+	if (_weapon == NULL){
+		std::cout << this->_name << "dose not have a weapon." << std::endl;
+	}
+	else{
+		std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+	}
 }
 
 void HumanB::setWeapon(Weapon &weapon){
